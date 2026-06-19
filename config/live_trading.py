8a48f8d004a3e0c35e25c7e15e6002f0667d5e8f
Reda <reda@macbookpro.mynetworksettings.com>
2026-06-19 00:28:08 -0400

    minor changes

--- config/live_trading.py
+++ config/live_trading.py
@@ -193,13 +193,17 @@ HOTPATH_EXECUTION_POLICY = {
                     },
     },
     "tennis": {
-        "size_shares": 3000.0,
+        "size_shares": 3750.0,
         "time_in_force": "GTC",
         "require_presign": True,
         "limit_price": 0.99,
-        "secondary_amount_usdc": 3000.0,
+        "secondary_amount_usdc": 3750.0,
         "secondary_time_in_force": "FAK",
         "secondary_limit_price": 0.99,
+        "market_overrides": {
+	    "tennis_set_totals": {"size_shares": 500.0, "limit_price": 0.99,
+                       "secondary_amount_usdc": 500.0, "secondary_limit_price": 0.99,},
+        },
         "retirement": {
             "size_shares": 100.0,
             "time_in_force": "GTC",
@@ -208,6 +212,7 @@ HOTPATH_EXECUTION_POLICY = {
             "secondary_time_in_force": "FAK",
             "secondary_limit_price": 0.49,
         },
+
     },
     "cs2":{
         "size_shares": 5.0,
